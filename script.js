2ce6991ff2a7436cff9574c4a916ee88fe6b846f
Lucas Fatec <lucas.oliveira437@fatec.sp.gov.br>
2024-09-23 21:55:15 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let valor1 = document.getElementById("entrada1").value;
+let valor2 = document.getElementById("entrada2").value;
+
+function somar(valor1, valor2){
+    document.getElementById("entrada3").value = valor1 + valor2;
+}
